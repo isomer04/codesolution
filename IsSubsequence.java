eb39e44d76b89e8ac10f5252e5ed4049021f5f79
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-07-10 16:25:02 -0400

    Create IsSubsequence.java

---
+++ IsSubsequence.java
@@ -0,0 +1,18 @@
+// Is Subsequence
+
+
+ public static boolean isSubsequence(String s, String t) {
+        int sIndex = 0;
+        int tIndex = 0;
+        
+        while(sIndex < s.length() && tIndex < t.length()) {
+            if(s.charAt(sIndex) == t.charAt(tIndex)){
+                sIndex++;
+            }
+            
+            tIndex++;
+        }
+        
+        return sIndex == s.length();
+
+    }
