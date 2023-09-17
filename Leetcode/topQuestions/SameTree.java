c8cdfc1ed598568cc64eeab0acdcee16a0bf0210
isomer04 <43922158+isomer04@users.noreply.github.com>
2023-09-16 22:08:25 -0400

    SameTree java solution

---
+++ Leetcode/topQuestions/SameTree.java
@@ -0,0 +1,18 @@
+package Leetcode.topQuestions;
+
+
+
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
